cd71d918499d112cc69daba8261f90c1736e2b3e
Mathis W <mathis.wan@outlook.com>
2017-05-06 14:00:04 -0400

    add timer

--- app/components/utils/clock.js
+++ app/components/utils/clock.js
@@ -4,24 +4,36 @@
 'use strict';
 
 angular.module('myApp.clock',[]).directive('appClock',['$interval',function ($interval) {
-    return function(scope, element, attrs){
-        var format, stopTime;
-        var totalSeconds = 0;
+    return {
+        template:"<div>Spent Time: {{totalSeconds}}  seconds." +
+        "<input id='timerBtn' type='button' value='start' ng-click='toggle();'></div>",
 
-        function updateTime(){
-            totalSeconds = totalSeconds + 1;
-            element.text("Spent Time:  "+totalSeconds + " seconds.");
+        link: function(scope, element, attrs){
+            var format, stopTime;
+            var isStarted = false;
+            scope.totalSeconds = 0;
+
+            function updateTime(){
+                scope.totalSeconds = scope.totalSeconds + 1;
+            }
+
+            scope.toggle = function(){
+                var timerBtn = document.getElementById('timerBtn');
+                if(isStarted){
+                    $interval.cancel(stopTime);
+                    isStarted = false;
+                    angular.element(timerBtn).val('start');
+                }else{
+                    scope.totalSeconds = 0;
+                    stopTime = $interval(updateTime,1000);
+                    isStarted = true;
+                    angular.element(timerBtn).val('stop');
+                }
+            }
+
+            element.on('$destory',function(){
+                $interval.cancel(stopTime);
+            });
         }
-
-        scope.$watch(attrs.appClock, function(value){
-            updateTime();
-        });
-
-        stopTime = $interval(updateTime,1000);
-
-        element.on('$destory',function(){
-            $interval.cancel(stopTime);
-        });
     }
-
 }]);
